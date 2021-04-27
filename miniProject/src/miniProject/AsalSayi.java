2a2eb11c88b81f0bfb1dcc627bf5d928f0d80411
byr4g <byr4g@LAPTOP-SV3BG1BE>
2021-04-27 10:37:47 +0300

    miniProject

---
+++ miniProject/src/miniProject/AsalSayi.java
@@ -0,0 +1,36 @@
+package miniProject;
+
+public class AsalSayi {
+
+	public static void main(String[] args) {
+		int number = 7; //<<<-- Sonucu ogrenmek istediginiz Sayiyi buraya giriniz
+		int remainder = number % 2;
+		//System.out.println(remainder);
+		boolean isPrime = true;
+		
+		if (number==1) {
+			System.out.println("Sayi asal degildir");
+			return;
+		}
+		
+		if(number<1){
+			System.out.println("Sayi gecersiz");
+			
+		}
+		
+		for (int i=2; i<number; i++) {
+			if (number % i == 0) {
+				isPrime = false;
+			}
+		}
+		if (isPrime) {
+			System.out.println("Sayi asaldir");
+			
+		}
+		else{
+			System.out.println("Sayi asal degildir");
+		}
+
+	}
+
+}
