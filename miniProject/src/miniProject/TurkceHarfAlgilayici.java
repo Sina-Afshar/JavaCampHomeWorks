ac8327044a6e14c1333847c1e9a6201d9cea3415
byr4g <byr4g@LAPTOP-SV3BG1BE>
2021-04-27 13:51:40 +0300

    TurkceHarfAlgilayici

--- miniProject/src/miniProject/TurkceHarfAlgilayici.java
+++ miniProject/src/miniProject/TurkceHarfAlgilayici.java
@@ -5,18 +5,18 @@ public class TurkceHarfAlgilayici {
 	public static void main(String[] args) {
 		char harf = 'W';
 		switch (harf) {
-		case'A', 'I','O','U' :
+		case'A','I','O','U','a','ý','u','o':
 			System.out.println("Girilen harf kalýn sesli harftir");
 			break;
 		
-		case'E', 'Ý','Ö','Ü' :
+		case'E','Ý','Ö','Ü','e','i','ö','ü' :
 			System.out.println("Girilen harf ince sesli harftir");
 			return;
-		case 'T', 'Þ', 'S', 'P', 'K', 'H', 'F', 'Ç':
+		case 'T','Þ','S','P','K','H','F','Ç','t','þ','s','p','k','h','f','ç':
 			System.out.println("Girilen harf sert sessiz harftir");
 			return;
 			
-		case 'V', 'Y', 'Z', 'R', 'N', 'M', 'L', 'J', 'Ð', 'G', 'D', 'C', 'B' :
+		case 'V','Y','Z','R','N','M','L','J','Ð','G','D','C','B','v','y','z','r','n','m','l','j','ð','g','d','c','b':
 			System.out.println("Girilen harf yumuþak sessiz harftir");
 			return;
 			
