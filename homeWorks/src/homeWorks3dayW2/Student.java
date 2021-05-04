38d09fd3d63bebe5583c8743e177a341e728fb40
byr4g <byr4g@LAPTOP-SV3BG1BE>
2021-05-04 14:42:27 +0300

    .

--- homeWorks/src/homeWorks3dayW2/Student.java
+++ homeWorks/src/homeWorks3dayW2/Student.java
@@ -3,12 +3,13 @@ package homeWorks3dayW2;
 public class Student extends User {
 	private int studentNo;
 
+	public Student(){}
+	
 	public Student(int id, String firstName, String lastName, String eMail, int studentNo) {
 		super(id, firstName, lastName, eMail);
 		this.studentNo = studentNo;
 	}
 	
-	public Student(){}
 	
 	public int getStudentNo() {
 		return studentNo;
